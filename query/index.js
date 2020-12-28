83b944a7ea8e6d4adda9d22c7f47123fb365db42
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-12-28 20:35:45 +0000

    implementing event sync

--- query/index.js
+++ query/index.js
@@ -9,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", async (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
 
@@ -41,10 +35,28 @@ app.post("/events", async (req, res) => {
     comment.content = content;
     comment.status = status;
   }
+};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event of res.data) {
+    console.log("Processing event:", event.type);
+
+    handleEvent(event.type, event.data);
+  }
 });
